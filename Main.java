cde9c8545be20318bec865ab0edf9a80c8ff90a7
pietrademicianoEdu <pietra.demiciano@edu.unifil.br>
2026-06-16 22:52:20 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.print("Digite o segundo número decimal: ");
         double numB = scanner.nextDouble();
 
-        System.out.println("\n--- Conversão Individual ---");
+        System.out.println("\n---- Conversão Individual ----");
         
         // Converte o primeiro número
         String[] resA = CalculaIEEE754.converterParaBinario(numA);
@@ -21,7 +21,7 @@ public class Main {
         System.out.println("Mantissa: " + resA[2]);
         System.out.println("Binário 32 bits: " + resA[0] + " " + resA[1] + " " + resA[2]);
 
-        // Converte o segundo
+        // Converte o segundo número
         String[] resB = CalculaIEEE754.converterParaBinario(numB);
         System.out.println("\nAnalisando o número B: " + numB);
         System.out.println("Sinal:    " + resB[0] + (resB[0].equals("1") ? " (Negativo)" : " (Positivo)"));
@@ -29,12 +29,12 @@ public class Main {
         System.out.println("Mantissa: " + resB[2]);
         System.out.println("Binário 32 bits: " + resB[0] + " " + resB[1] + " " + resB[2]);
 
-        // Operação de soma e comparação
+        // operação de soma e comparação
         System.out.println("\n==================================================");
         double somaReal = numA + numB;
 
         System.out.println("Operação solicitada: " + numA + " + " + numB);
-        System.out.println("Resultado Decimal (Operação normal do Java): " + somaReal);
+        System.out.println("Resultado Decimal: " + somaReal);
 
         // Converte o resultado da soma de volta para o padrão IEEE 754
         String[] resSoma = CalculaIEEE754.converterParaBinario(somaReal);
